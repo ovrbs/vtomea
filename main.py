bafe718d30084295ff3097b2992c2ef0e97b71bb
ovrbs <ovrbs@mail.ru>
2024-11-23 23:42:13 +0300

    2nd

---
+++ main.py
@@ -0,0 +1,20 @@
+# 20241018
+# зшз
+# https://sky.pro/wiki/python/python-dlya-android-rukovodstvo-dlya-nachinayushih/
+# https://github.com/kivy/buildozer?tab=readme-ov-file
+# https://habr.com/ru/articles/301776/
+# https://ru.stackoverflow.com/questions/pytho1499638/%D0%9A%D0%B0%D0%BA-%D1%81%D0%BE%D0%B7%D0%B4%D0%B0%D1%82%D1%8C-apk-%D1%84%D0%B0%D0%B9%D0%BB-%D1%81-%D0%BF%D0%BE%D0%BC%D0%BE%D1%89%D1%8C%D1%8E-buildozer-%D0%B2-windows
+
+'''
+from kivy.app import App
+from kivy.uix.label import Label
+
+class MyApp(App):
+    def build(self):
+        return Label(text="git pull!")
+
+if __name__ == "__main__":
+    MyApp().run()
+'''
+
+print('!')
